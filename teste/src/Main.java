8086b818cc157213bb20a49249dfb26eb99cd87e
SoldadoHumano <157237604+SoldadoHumano@users.noreply.github.com>
2024-01-20 14:58:48 -0300

    Add files via upload

---
+++ teste/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter three numbers: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int higher = max(a, b, c);
+
+        showResult(higher);
+
+        scanner.close();
+    }
+
+    public static int max(int x, int y, int z) {
+        int aux;
+        if (x > y && x > z) {
+            aux = x;
+        }
+        else if (y > x) {
+            aux = y;
+        }
+        else {
+            aux = z;
+        }
+        return aux;
+    }
+
+    public static void showResult(int value) {
+        System.out.println("Higher = " + value);
+    }
+}
